a3a32213e9619165365950570c7cde2742d56a8a
Rahul Rawat <rahulrawat4060@gmail.com>
2025-08-17 14:59:16 +0530

    BE done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 dotenv.config();
 
 import authRouter from './routes/authRoutes.js';
 import connectDB from './utils/connectDB.js';
 import recordingRouter from './routes/recordingRoutes.js';
+import validateURL from './middlewares/validateURL.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
+app.use(validateURL);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

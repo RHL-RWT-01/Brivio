89d9ae417badb06755b604c6b0086ec5f9ae5d6f
Rahul Rawat <rahulrawat4060@gmail.com>
2025-08-17 14:55:20 +0530

    Route to get curr user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { login, signup } from '../controllers/authController.js';
+import { getMe, login, signup } from '../controllers/authController.js';
+import { protectRoute } from '../middlewares/protectRoute.js';
 const authRouter = express.Router();
 
 authRouter.post('/login', login);
 authRouter.post('/signup', signup);
+authRouter.get('/me', protectRoute, getMe);
 
 export default authRouter;
--- server/routes/recordingRoutes.js
+++ server/routes/recordingRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { createRecording, getAllRecordings, getRecordingById } from '../controllers/recordingController.js';
+import { createRecording, getAllRecordings, getRecordingById, processing } from '../controllers/recordingController.js';
 
 const recordingRouter = express.Router();
 
-recordingRouter.post('/', protectRoute, createRecording);
+recordingRouter.post('/new', protectRoute, createRecording);
+recordingRouter.get('/processing', protectRoute, processing);
 recordingRouter.get('/all', protectRoute, getAllRecordings);
 recordingRouter.get('/:id', protectRoute, getRecordingById);
 
